ede15409a311ade6dc2f23ae9a5f778b4b7a189a
daraghkearney <g00438215@atu.ie>
2025-10-07 15:00:55 +0100

    Created FuelStationSearch class

---
+++ FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/FuelStationSearch.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdproject.ffmainapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FuelStationSearch {
+    private String stationName;
+    private String location;
+    private double petrolPrice;
+    private double dieselPrice;
+}
