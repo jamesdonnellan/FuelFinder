d89cf0b8d75bc3dc1ee657678f26aa2a6997e3e7
James Donnellan <G00434671@atu.ie>
2025-10-07 15:06:26 +0100

    Added user data

--- FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/UserInformation.java
+++ FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/UserInformation.java
@@ -1,5 +1,14 @@
 package ie.atu.cicdproject.ffmainapp;
 
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UserInformation
 {
+    @NotBlank // Ensures that the fields cannot be left blank
+    private String userName, email, phone, userID;
 }
