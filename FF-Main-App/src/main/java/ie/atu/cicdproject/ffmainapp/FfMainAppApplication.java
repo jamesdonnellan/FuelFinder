1c52152c245026b7c3d49fb9329b8c2bc8ae1d45
James Donnellan <G00434671@atu.ie>
2025-10-07 14:03:12 +0100

    Created CI/CD Project - Fuel Finder

---
+++ FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/FfMainAppApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdproject.ffmainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfMainAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfMainAppApplication.class, args);
+    }
+
+}
---
+++ FF-Main-App/src/test/java/ie/atu/cicdproject/ffmainapp/FfMainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdproject.ffmainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfMainAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
