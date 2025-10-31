2304289805c69ae4874287926ab6e60c6e1aeb65
James Donnellan <G00434671@atu.ie>
2025-10-31 11:59:53 +0000

    Added update and delete to UserController. Created test cases for local testing

--- FF-Main-App/src/test/java/ie/atu/cicdproject/ffmainapp/UserTesting.java
+++ FF-Main-App/src/test/java/ie/atu/cicdproject/ffmainapp/UserTesting.java
@@ -1,4 +1,36 @@
 package ie.atu.cicdproject.ffmainapp;
 
-public class UserTesting {
+import ie.atu.cicdproject.ffmainapp.Services.UserService;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+public class UserTesting
+{
+    private UserService Uservice;
+
+    @BeforeEach
+    void setup()
+    {
+        Uservice = new UserService(); // before each test, create a new instance of passenger service
+    }
+
+    @Test
+    void createThenFindById()
+    {
+        UserInformation u = UserInformation.builder()
+                .userID("J7")
+                .userName("Jim")
+                .email("j@email.com")
+                .build();
+
+            Uservice.create(u);
+
+            Optional<UserInformation> found = Uservice.findById("J7");
+            assertTrue(found.isPresent());
+            assertEquals("Jim", found.get().getUserName());
+    }
 }
