8e6847203137d69db2a1ec7b380c53d7cf2b2be3
daraghkearney <g00438215@atu.ie>
2025-10-31 09:22:40 +0000

    Added to FuelStationController + Added validation annotations to FuelStationInformation

--- FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/FuelStationInformation.java
+++ FF-Main-App/src/main/java/ie/atu/cicdproject/ffmainapp/FuelStationInformation.java
@@ -1,4 +1,6 @@
 package ie.atu.cicdproject.ffmainapp;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,9 +11,16 @@ import lombok.Data;
 
 public class FuelStationInformation {
 
+    @NotBlank
     private String stationName;
+
+    @DecimalMin(value = "1.0")
     private double petrolPrice;
+
+    @DecimalMin(value = "1.0")
     private double dieselPrice;
+
+    @NotBlank
     private String location;
 
 
